853fe9ac622293d0d2430d3a1248696fa3078102
[tishpr] <[tishsny@gmail.com]>
2015-06-04 21:46:57 -0700

    added heroku url to product email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+   # http://bloccit.herokuapp.com
+   config.action_mailer.default_url_options = { host:'blocipedia-tish.herokuapp.com' } 
 end
